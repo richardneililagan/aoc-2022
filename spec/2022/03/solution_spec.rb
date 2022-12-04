5a71fa07897d4e48299752433b6b7cc59067aac9
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2022-12-04 11:47:30 +0800

    feat: adds solutions to 3.1 and 3.2

--- spec/2022/03/solution_spec.rb
+++ spec/2022/03/solution_spec.rb
@@ -1,32 +1,31 @@
 # frozen_string_literal: true
-require 'spec_helper'
+
+require "spec_helper"
 
 RSpec.describe Year2022::Day03 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2022/03/input.txt")) }
   let(:example_input) {
     <<~EOF
-      change
-      me
+      vJrwpWtwJgWrhcsFMMfFFhFp
+      jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+      PmmdzqPrVvPwwTWBwg
+      wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+      ttgJtRGJQctTZtZT
+      CrZsJsPPZsGzwwsLwLmpwMDw
     EOF
   }
 
   describe "part 1" do
-    it "returns nil for the example input" do
-      expect(described_class.part_1(example_input)).to eq(nil)
-    end
-
-    it "returns nil for my input" do
-      expect(described_class.part_1(input)).to eq(nil)
+    it "returns correct answers for examples" do
+      solution = described_class.part_1(example_input)
+      expect(solution).to eq(157)
     end
   end
 
   describe "part 2" do
-    it "returns nil for the example input" do
-      expect(described_class.part_2(example_input)).to eq(nil)
-    end
-
-    it "returns nil for my input" do
-      expect(described_class.part_2(input)).to eq(nil)
+    it "returns correct answers for examples" do
+      solution = described_class.part_2(example_input)
+      expect(solution).to eq(70)
     end
   end
 end
