27068fc824413e830144f92651fe029d7f992343
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2022-12-08 20:57:57 +0800

    feat: adds solution for 8.1 and 8.2

---
+++ challenges/2022/08/solution.rb
@@ -0,0 +1,118 @@
+# frozen_string_literal: true
+
+module Year2022
+  class Grid
+    attr_reader :visible_map
+    attr_reader :scenic_scores
+
+    def initialize(input_area)
+      area, visible_map, scenic_scores = parse_input(input_area)
+
+      @area = area
+      @visible_map = visible_map
+      @scenic_scores = scenic_scores
+    end
+
+    def determine_visibility
+      @area.each_with_index do |row, j|
+        parse_row_visibility j, 0, row.length - 1
+        parse_row_visibility j, row.length - 1, 0
+      end
+
+      (0..(@area.map(&:length).max - 1)).each do |i|
+        parse_column_visibility i, 0, @area.length - 1
+        parse_column_visibility i, @area.length - 1, 0
+      end
+    end
+
+    def determine_scenic_scores
+      i_max = @area.map(&:length).max
+      j_max = @area.length
+
+      (1..(j_max - 2)).each do |j|
+        (1..(i_max - 2)).each do |i|
+          tree = @area[j][i]
+
+          left = @area[j].first(i).reverse
+          right = @area[j].last(i_max - i - 1)
+          up = (@area.first j).map { |row| row[i] }.reverse
+          down = @area.last(j_max - j - 1).map { |row| row[i] }
+
+          directional_scores = [left, right, up, down].map do |direction|
+            return 0 if direction.empty?
+
+            visible_trees = direction.take_while { |t| t < tree }
+            visible_count = visible_trees.length
+
+            [visible_count + 1, direction.length].min
+          end
+
+          @scenic_scores[j][i] = directional_scores.reduce { |a, n| a * n }
+        end
+      end
+    end
+
+    private
+
+    def parse_input(input_area)
+      area = []
+      visible_map = []
+      scenic_scores = []
+
+      input_area.each do |row|
+        area << row.chars.map(&:to_i)
+        visible_map << Array.new(row.length).map { false }
+        scenic_scores << Array.new(row.length).map { 0 }
+      end
+
+      [area, visible_map, scenic_scores]
+    end
+
+    def parse_row_visibility(j, from, to)
+      current_max_height = -1
+
+      range = (from < to) ? (from..to) : from.downto(to)
+
+      range.each do |i|
+        current_tree_height = @area[j][i]
+        if current_tree_height > current_max_height
+          current_max_height = current_tree_height
+          @visible_map[j][i] = true
+        end
+      end
+    end
+
+    def parse_column_visibility(i, from, to)
+      current_max_height = -1
+
+      range = (from < to) ? (from..to) : from.downto(to)
+
+      range.each do |j|
+        current_tree_height = @area[j][i]
+        if current_tree_height > current_max_height
+          current_max_height = current_tree_height
+          @visible_map[j][i] = true
+        end
+      end
+    end
+  end
+
+  class Day08 < Solution
+    # @input is available if you need the raw data input
+    # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
+
+    def part_1
+      grid = Grid.new data
+      grid.determine_visibility
+      grid.visible_map.flatten.count { |tree| tree }
+    end
+
+    def part_2
+      grid = Grid.new data
+      grid.determine_scenic_scores
+      grid.scenic_scores.flatten.max
+    end
+
+    private
+  end
+end
---
+++ spec/2022/08/solution_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Year2022::Day08 do
+  let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2022/08/input.txt")) }
+  let(:example_input) {
+    <<~EOF
+      30373
+      25512
+      65332
+      33549
+      35390
+    EOF
+  }
+
+  describe "part 1" do
+    it "returns correct answers for provided samples" do
+      solution = described_class.part_1(example_input)
+      expect(solution).to eq(21)
+    end
+  end
+
+  describe "part 2" do
+    it "returns correct answers for provided samples" do
+      solution = described_class.part_2(example_input)
+      expect(solution).to eq(8)
+    end
+  end
+end
